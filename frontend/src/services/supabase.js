bed1d3cdd5bd9475a9a864abeaa4544a675dfbc3
johnyarabe-create <johnyarabe@gmail.com>
2026-07-18 23:49:21 -0400

    Update supabase.js

--- frontend/src/services/supabase.js
+++ frontend/src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://wmtpsryktpunkuklnqpas.supabase.co';
+const supabaseUrl = 'https://wmtpsryktpunkuklnqpa.supabase.co';
 const supabaseKey = 'sb_publishable_epHkdfzRZJYnMBJW6HUPVw_HC4EpQsX';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
